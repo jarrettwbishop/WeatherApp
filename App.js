a814d99de792cbc2793ee18c77f5ea4f44824b88
Jarrett Bishop <jarrettwbishop@gmail.com>
2023-06-04 13:47:19 -0400

    Restructure code

--- App.js
+++ App.js
@@ -2,32 +2,25 @@ import React, { useState, useEffect } from 'react'
 import { ActivityIndicator, StyleSheet, View} from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import Tabs from './src/components/Tabs'
-import * as Location from 'expo-location'
-import { WEATHER_API_KEY } from '@env'
 import { useGetWeather } from './src/hooks/useGetWeather'
 
 // api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
 const App = () => {
   const [loading, error, weather] = useGetWeather()
-  console.log(loading, error, weather)
-  
-  if (weather) {
-    console.log(weather);
-  }
 
-  if (loading) {
+  if(weather && weather.list) {
     return (
-      <View style={styles.container}>
-        <ActivityIndicator size={'large'} color={'blue'}/>
-      </View>
+      <NavigationContainer>
+        <Tabs weather={weather} />
+      </NavigationContainer>
     )
   }
 
   return (
-    <NavigationContainer>
-      <Tabs />
-    </NavigationContainer>
+    <View style={styles.container}>
+      <ActivityIndicator size={'large'} color={'blue'}/>
+    </View>
   )
 }
 
